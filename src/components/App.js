f1e37b69cde82ad05f9657fb3cad919a1e6194d7
Rikukuku <ricky_gland821_0714@yahoo.co.jp>
2020-02-20 23:05:21 +0900

    Implement event creator

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, { useState, useReducer } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import reducer from '../store/reducers'
+import { CREATE_EVENT } from '../store/actions'
 
-const App = () => (
-  <div className="container-fluid">
+const App = () => {
+  const [state, dispath] = useReducer(reducer, [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const createEvent = e => {
+    e.preventDefault()
+    dispath(
+      {
+        type: CREATE_EVENT,
+        title: title,
+        body:body
+      }
+    )
+    setTitle('')
+    setBody('')
+  }
+  console.log(state)
+  return (
+    <div className="container-fluid">
     <h4>イベント作成画面</h4>
     <div className='form-group'>
-      <label　htmlFor="formEventTitle">タイトル</label>
-      <input className="form-control" id="formEventTitle" />
+      <label　htmlFor="formEventTitle">Title</label>
+      <input className="form-control" id="formEventTitle" value={title} onChange={(e) => setTitle(e.target.value)}/>
     </div>
 
     <div className='form-group'>
-      <label　htmlFor="formEventBody">ボデイー</label>
-      <textarea className="form-control" id="formEventBody" />
+      <label　htmlFor="formEventBody">Body</label>
+      <textarea className="form-control" id="formEventBody" value={body} onChange={(e) => setBody(e.target.value)}/>
     </div>
 
-    <button className="btn btn-primary">イベントを作成する</button>
+    <button className="btn btn-primary" onClick={createEvent}>イベントを作成する</button>
     <button className="btn btn-danger">イベントを全て削除する</button>
 
     <table className="table table-hover">
@@ -27,9 +47,11 @@ const App = () => (
         </tr>
       </thead>
       <tbody>
-        
+
       </tbody>
     </table>
   </div>
-)
+  )
+
+}
 export default App;
---
+++ src/store/actions/index.js
@@ -0,0 +1,5 @@
+export const CREATE_EVENT = 'CREATE_EVENT'
+
+export const createEvent = () => ({
+  type: CREATE_EVENT
+})
